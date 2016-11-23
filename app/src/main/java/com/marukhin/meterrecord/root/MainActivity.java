02e380e0c7493dcd6d04a70ca6fb2d197b00f930
Mikhail <mmteams91java@gmail.com>
2016-11-23 13:25:41 +0300

    initial commit

---
+++ app/src/main/java/com/marukhin/meterrecord/root/MainActivity.java
@@ -0,0 +1,15 @@
+package com.marukhin.meterrecord.root;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.marukhin.meterrecord.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
